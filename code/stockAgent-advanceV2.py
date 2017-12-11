46325332e4c18918ea404283d45a19119a968199
Song, Yixiao <yixiao.song@sap.com>
2017-12-11 13:29:10 -0800

    add comments

--- code/stockAgent-advanceV2.py
+++ code/stockAgent-advanceV2.py
@@ -1,40 +1,24 @@
 
 
-
+'''
 # --- enable this to run on GPU
 # import os
 #
 # os.environ['THEANO_FLAGS'] = "device=cuda*,floatX=float32"
-
+'''
 import random, numpy, math,csv,scipy
 from SumTree import SumTree
 
 from datetime import datetime ,timedelta
 import pandas_datareader as pdr
-# -------------------- BRAIN ---------------------------
 from keras.models import Sequential
 from keras.layers import *
 from keras.optimizers import *
 
 
-# #----------
-# HUBER_LOSS_DELTA = 1.0
-
-# HUBER_LOSS_W=1
-#
-# #----------
-# def huber_loss(y_true, y_pred):
-#     err = y_true - y_pred
-#
-#     cond = K.abs(err) < HUBER_LOSS_DELTA
-#     L2 = 0.5 * K.square(err)
-#     L1 = HUBER_LOSS_DELTA * (K.abs(err) - 0.5 * HUBER_LOSS_DELTA)
-#
-#     loss = tf.where(cond, L2, L1)   # Keras does not cover where function in tensorflow :-(
-#
-#     return K.mean(loss)
-    # return tf.losses.huber_loss(y_true,y_pred,weights=HUBER_LOSS_W,delta=HUBER_LOSS_DELTA, reduction=None)
-
+'''
+# -------------------- BRAIN ---------------------------
+'''
 class Brain:
     def __init__(self, stateCnt, actionCnt):
         self.stateCnt = stateCnt
@@ -46,6 +30,7 @@ class Brain:
 
     def _createModel(self):
         model = Sequential()
+        """
         # #of layer
         # here the input layer have statecnt state and 64 units and middle layer have 128 units , will be much faster if reduce layer but it will got little unstable
         #The Brain class encapsulates the neural network. Our problem is simple enough so we will use only one hidden layer of 64 neurons,
@@ -53,8 +38,15 @@ class Brain:
         # Their activation function will be linear. Remember that we are trying to approximate the Q function,
         #Instead of simple gradient descend, we will use a more sophisticated algorithm RMSprop, and Mean Squared Error (mse) loss function.
 
+        below is example to change the model,you can change the actionvation function or unit of paramter ,or add new layer
+        like:
+                # model.add(BatchNormalization())
+
+"""
 
         model.add(Dense(units=64, activation='relu', input_dim=stateCnt))
+        # model.add(BatchNormalization())
+
         model.add(Dense(units=128, activation='relu', input_dim=64))
         model.add(Dense(units=actionCnt, activation='linear'))
 
@@ -78,8 +70,9 @@ class Brain:
 
     def updateTargetModel(self):
         self.model_.set_weights(self.model.get_weights())
-
+'''
 # -------------------- MEMORY --------------------------
+'''
 class Memory:  # stored as ( s, a, r, s_ )
     samples = []
     e = 0.01
@@ -114,9 +107,12 @@ class Memory:  # stored as ( s, a, r, s_ )
         self.tree.update(idx, p)
     # def isFull(self):
     #     return len(self.samples) >= self.capacity
-
+'''
 # -------------------- AGENT ---------------------------
-MEMORY_CAPACITY = 400000
+MEMORY_CAPACITY: how many s this memory should store
+
+'''
+MEMORY_CAPACITY = 100000
 BATCH_SIZE = 64
 
 GAMMA = 0.99
@@ -226,15 +222,19 @@ class Stock:
         self.company=company
         self.transactionFee=TRANSACTION_FEE
         self.enddate=enddate
-        self.isCached=False
+        self.isCached=[]
         self.maxindex=float('+inf')
-        self.cacheStock= None
-        # self.env = gym.make(problem)
+        self.cacheStock= {}
     def calcReward(self,cash,stock,price):
         return cash+stock*price
 
     def stockTransaction(self,cash,stock,index,action):
+        """
+        stockTransaction return  feature s and Reward
+
         # action : sell, buy , hold
+        in case need extend action list:  eg half sell or buy , need to extent actionNum and case statement below
+        """
         stockPrice=float(self.closePrice(self.company,index))
         currentValue=self.calcReward(cash,stock,stockPrice)
         if action == 2 :
@@ -244,13 +244,6 @@ class Stock:
         if action ==0:
             return [cash+stock*stockPrice-self.transactionFee,0,stockPrice],self.calcReward(cash+stock*stockPrice-self.transactionFee,0,stockPrice)
 
-    # def step(self,s,a,date):
-    #     if date+self.stepTimeDelta>self.enddate:
-    #         return s,0,True,"pass today"
-    #     # Cash, Stock ,current stock price
-    #     s_,r=self.stockTransaction(s[0],s[1],date,a)
-    #
-    #     return s_,r,False,''
     def step(self,s,a,index):
         reward=self.calcReward(s[0],s[1],s[2])
 
@@ -264,15 +257,32 @@ class Stock:
 
         return s_,r,False,''
 
-    def closePrice(self,company,index):
-        if not self.isCached:
-            self.cacheStock=pdr.get_data_yahoo(symbols=company, start=self.date, end=self.enddate)['Close']
-            self.maxindex=self.cacheStock.__len__()-1
-            print ('start cache index for company %s from date %s to date %s ,total number of record %s')%(self.company,self.date.strftime('%Y-%m-%d'),self.enddate.strftime('%Y-%m-%d'),self.maxindex+1)
-            self.isCached=True
-        return self.cacheStock[index]
+
+    def closePrice(self, company, index):
+        """
+            close price will get data from internet at first run and cache it for future request,
+            right now is close price for certain company, and you can change to other price by change 'Close' below.
+            This function able to cache more than one stock, in case need use nasdaq index or other index, you can use that company/index name as company name below
+
+
+        """
+        if company not in self.isCached:
+            self.cacheStock[company] = pdr.get_data_yahoo(symbols=company, start=self.date, end=self.enddate)['Close']
+            self.maxindex = self.cacheStock[company].__len__() - 1
+            print ('start cache index for company %s from date %s to date %s ,total number of record %s') % (
+            company, self.date.strftime('%Y-%m-%d'), self.enddate.strftime('%Y-%m-%d'), self.maxindex + 1)
+            self.isCached.append(company)
+        return self.cacheStock[company][index]
+
 
     def run(self, agent,printout=False):
+        """
+        Main function here ,
+        a is action (0- #of action)
+        S_ is future step
+        below is to init s, if need to extent s to hold more value for each stage, you will need to change below s and s in stockTransaction ,also stateCnt in the main controler
+
+        """
         s=[self.investment,0,self.closePrice(self.company,0)]
         s=numpy.array(s)
         reward = self.investment
@@ -305,33 +315,54 @@ class Stock:
 
 
 
+
+
+"""
 # -------------------- MAIN ----------------------------
+
+This is main control , company is the name of company which will be used for API request to get stock statistic
+
+STOP_INVERST_PREC is the precentage which the agent will stop trying.
+
+TRANSACTION_FEE: each buy or sell cost
+initalInvestment : inital investment
+
+stateCnt = 3  # Cash, Stock ,current stock price
+define how many stat that the DQN input layer have
+
+
+actionCnt = 3  # sell, buy , hold
+define how may action the agent cant take (init from 0-2  in this case)
+
+
+
+
+"""
 company = 'SAP'
 # tartdate=datetime(2015,01,01)
 
 # enddate=datetime.now()
 
-startdate=datetime(2016,1,1)
-
+startdate=datetime(2000,1,1)
 enddate=datetime(2017,1,1)
-# stop when you cash value is only 50% of your total cash
-
-STOP_INVERST_PREC=float(0.5)
+STOP_INVERST_PREC=float(0.6)
 TRANSACTION_FEE=2
 LEARNING_RATE = 0.00025
-needrepaly=True
+needrepaly=False
 replayFile='stock_SAP_episode30955_2017-12-06.h5'
 initalInvestment=1000
-MYLIST=[]
-
-#___________________________________ above is control parameter ________________________
-
-stock = Stock(initalInvestment,startdate,enddate,company)
 
 stateCnt = 3  # Cash, Stock ,current stock price
 actionCnt = 3  # sell, buy , hold
+EVALUTION_PERIODS=1000
+NEED_EVAL=False
+"""
+#___________________________________ above is control parameter ________________________
+"""
 
 
+stock = Stock(initalInvestment,startdate,enddate,company)
+MYLIST=[]
 agent = Agent(stateCnt, actionCnt)
 randomAgent = RandomAgent(actionCnt)
 count = 0
@@ -341,6 +372,9 @@ try:
        agent.brain.model.load_weights(replayFile)
 
     else:
+        """
+        use random agent  to initialize paramter pool
+        """
         while randomAgent.exp < MEMORY_CAPACITY:
             stock.run(randomAgent)
         print('use random agent to initial model %s tims'%(randomAgent.exp))
@@ -351,12 +385,16 @@ try:
         count += 1
         print("episode # %s" % (count))
         stock.run(agent,printout=True)
-
+        if NEED_EVAL and not bool(count % EVALUTION_PERIODS):
+            fileName = "savepoint_stock_%s_episode%s_%s.h5" % (company, count, datetime.now().strftime('%Y-%m-%d'))
+            """
+            TODO : need value evaluator to measure the performance at certain point ,eg:every 1000 steps or so
+            """
 finally:
     agent.brain.model.summary();
 
-    agent.brain.model.save("stock_%s_episode%s_%s.h5"%(company,count,datetime.now().strftime('%Y-%m-%d')))
-    with open("advv2_stock_%s_episode%s_%s.csv"%(company,count,datetime.now().strftime('%Y-%m-%d')), 'wb') as myfile:
+    agent.brain.model.save("advv2_stock_%s_Final_episode%s_%s.h5" % (company, count, datetime.now().strftime('%Y-%m-%d')))
+    with open("advv2_stock_%s_Final_episode%s_%s.csv"%(company,count,datetime.now().strftime('%Y-%m-%d')), 'wb') as myfile:
         wr = csv.writer(myfile, delimiter="\n")
         wr.writerow(MYLIST)
 
